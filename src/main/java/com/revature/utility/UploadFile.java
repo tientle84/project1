c8d9cb56ac2bcfb100924a039226d7c54ad92b64
tientle84 <alctdl00@gmail.com>
2022-05-03 02:17:30 -0400

    Update UploadFile

--- src/main/java/com/revature/utility/UploadFile.java
+++ src/main/java/com/revature/utility/UploadFile.java
@@ -1,13 +1,14 @@
 package com.revature.utility;
 
 // Imports the Google Cloud client library
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.*;
 
+import com.google.common.collect.Lists;
 import io.javalin.http.UploadedFile;
 import org.apache.commons.io.IOUtils;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.SecureRandom;
@@ -21,17 +22,13 @@ public class UploadFile {
         // The ID of your GCS bucket
         String bucketName = "project1-imgs";
 
-
+        // create random file name
         String fileName = fileNameGenerate(15);
         String objectName = "reimb_imgs/" + fileName + receiptFile.getExtension();
-        String contentType = receiptFile.getContentType();
-        InputStream inputStream = receiptFile.getContent();
-        byte[] bytes = IOUtils.toByteArray(inputStream);
 
-        Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-        BlobId blobId = BlobId.of(bucketName, objectName);
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(contentType).build();
-        storage.create(blobInfo, bytes);
+        // create bucket and upload the image file
+        Bucket bucket = getBucket(bucketName);
+        bucket.create(objectName, receiptFile.getContent(), receiptFile.getContentType());
 
         return "https://storage.googleapis.com/project1-imgs/" + objectName;
     }
@@ -44,5 +41,25 @@ public class UploadFile {
 
         IntStream intStream = secureRandom.ints(leftLimit, rightLimit+1).filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(fileNameSize);
         return intStream.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+    }
+
+    private static Bucket getBucket(String bucketName) {
+        try{
+            GoogleCredentials credentials = GoogleCredentials
+                    .fromStream(new FileInputStream(System.getenv("GOOGLE_APPLICATION_CREDENTIALS")))
+                    .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+
+            Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+            Bucket bucket = storage.get(bucketName);
+
+            if (bucket == null) {
+                throw new IOException("Bucket not found: " + bucketName);
+            }
+
+            return bucket;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
